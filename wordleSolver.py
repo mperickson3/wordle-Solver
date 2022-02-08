b55c9244cbb16157a61a7d71118212ad4cf40679
mperickson3 <mperickson3@gmail.com>
2022-02-08 17:36:57 -0600

    Added Incorrect Index filter and some wighting for duplicate letters

--- wordleSolver.py
+++ wordleSolver.py
@@ -1,4 +1,5 @@
 
+from ast import Break
 from copy import copy
 import string
 
@@ -11,6 +12,18 @@ def filterWordsIndex(word):
         return True
     else:
         return False
+
+def filterWordsWrongIndex(word):
+    count = 0
+    for key in letterWrongIndex:
+        if (key[0] == word[letterWrongIndex[key]]):
+            count+=1
+    if(count == 0):
+        return True
+    else:
+        return False
+
+#Add index filter to remove words that have letter index incorrectly
 
 def filterWordsContain (word):
     #print (all([letters in word for letters in lettersInWord]))
@@ -63,11 +76,7 @@ def removeBreak(word):
 
 linesFormatted = list(map(removeBreak, lines))
 
-# linesFormatted = ['apses', 'arses', 'ashes', 'asses', 'asset', 'bases', 'beses', 'bests', 'bises', 'bless', 'buses', 'cases', 'chess', 'coses', 'cress', 'desks', 'doses', 'dress', 'eases', 'easts', 'erses', 'esnes', 'essay', 'esses', 'eyass', 'fesse', 'fests', 'fosse', 'fuses', 'gases', 'gesso', 'gests', 'guess', 'hests', 'hoses', 'isles', 'issei', 'issue', 'jesse', 'jests', 'lases', 'leses', 'loess', 'loses', 'lyses', 'masse', 'mesas', 'messy', 'mises', 'muses', 'nests', 'noses', 'oases', 'pases', 'passe', 'pesos', 'pests', 'poses', 'posse', 'press', 'puses', 'rases', 'rests', 'rises', 'roses', 'ruses', 'sabes', 'sades', 'safes', 'sages', 'sakes', 'sales', 'sanes', 'sates', 'saves', 'saxes', 'seals', 'seams', 'sears', 'seats', 'sects', 'seeds', 'seeks', 'seels', 'seems', 'seeps', 'seers', 'segos', 'seifs', 'seise', 'seism', 'selfs', 'sells', 'semes', 'semis', 
-# 'sends', 'sensa', 'sense', 'septs', 'seres', 'serfs', 'setts', 'sexes', 'sexts', 'sheas', 'sheds', 'shews', 'shies', 'shoes', 'sices', 'sides', 'sikes', 'sines', 'sipes', 'sires', 'sises', 'sites', 'sixes', 'sizes', 'skees', 'skegs', 'skeps', 'skews', 'skies', 'sleds', 'slews', 'sloes', 'slues', 'smews', 'sneds', 'snyes', 'sokes', 'soles', 'sones', 'sores', 'souse', 'spaes', 'specs', 'spews', 'spies', 'spues', 'stems', 'steps', 'stets', 'stews', 'sties', 'styes', 'suers', 'suets', 'supes', 'syces', 'sykes', 'tasse', 'tests', 'tress', 'users', 'vases', 'vests', 'vises', 'wests', 'wises', 'yeses', 'zests']
-#print (linesFormatted)
 alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-# print (letters)
 i=0
 count = [0] * 26
 
@@ -88,53 +97,61 @@ for n in count:
 
 print (max(weightLetters))
 filteredWords = linesFormatted
-
-# print (ord('a')-97)
-for i in range(0,6):
+guessNum = 0
+while guessNum < 6:
 
     weightWords = []
     for word in filteredWords:
         prob = 1
+        weightLettersCopy = weightLetters[:]
         for letter in word:
-            prob*=weightLetters[(ord(letter)-97)]
+            prob*=weightLettersCopy[(ord(letter)-97)]
+            #Penalize repeated leters
+            weightLettersCopy[(ord(letter)-97)] = weightLettersCopy[(ord(letter)-97)] * 0.6
         weightWords.append(prob)
 
     guess1=filteredWords[weightWords.index(max(weightWords))]
     print (guess1)
 
-    print(type(linesFormatted))
+    #print(type(linesFormatted))
 
-    word = "Hello"
-    word.replace('H', '')
-    print(word)
 
     guess1Result = input('Please enter results: ')
+
+    if(guess1Result == "22222"):
+        guessNum=7
+        print ("YOU WINNNNNN!!!!!!!!!")
+        Break
 
     lettersInWord = []
     lettersNotInWord = []
     letterIndex = {}
+    letterWrongIndex = {}
 
     index = 0
 
-    for i in guess1Result:
-        #print (i)
-        if (int(i)>0):
+    for j in guess1Result:
+        if (int(j)>0):
             lettersInWord.append(guess1[index])
-        elif (int(i)==0):
+            if(int(j)==1):
+                letterWrongIndex[guess1[index]+str(index)] = index
+        elif (int(j)==0):
             lettersNotInWord.append(guess1[index])
-        if (int(i)==2):
+        if (int(j)==2):
             letterIndex[guess1[index]+str(index)] = index
 
         index+=1
 
-    print(letterIndex)
-    print (lettersInWord)
-    print (lettersNotInWord)
+    # print(letterIndex)
+    # print (lettersInWord)
+    # print (lettersNotInWord)
 
 
 
 
     filteredWords = list(filter(filterWordsContain, filteredWords))
     filteredWords = list(filter(filterWordsIndex, filteredWords))
-
+    filteredWords = list(filter(filterWordsWrongIndex, filteredWords))
     print(filteredWords)
+
+
